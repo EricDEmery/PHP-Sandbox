8b222cb92e9ed2a2af1779cd5685576a2de167a0
Eric Emery <ericemery099@gmail.com>
2023-12-25 02:54:44 +0000

    adding some PHP

---
+++ sandbox.php
@@ -0,0 +1,27 @@
+//PHP code
+
+<?php
+    $username = 'Eric';
+
+    $greeting = 'Hello, ' . $username . '.';
+
+    $offer = [ 
+        'item' => 'Chocolate',
+        'qty' => 5,
+        'price' => 5,
+        'discount' => 4,
+    ];
+
+    
+    ?>
+
+//Begin HTML
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>The Candy Store</title>
+
+        </head>
+
+    </html>
